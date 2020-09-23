7e1c3be1bd6a871cf12044544a21c1ce32a9dd08
CoolSnow <coolsnow2020@gmail.com>
2020-09-23 10:57:31 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+/*
+ * @Description: 
+ * @Author: CoolSnow (coolsnow2020@gmail.com)
+ * @Date: 2020-09-23 10:32:42
+ * @LastEditors: CoolSnow
+ * @LastEditTime: 2020-09-23 10:49:40
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'

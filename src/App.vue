282103f530f9410a8023f7147e82d463b65057bb
CoolSnow <coolsnow2020@gmail.com>
2020-09-24 23:19:55 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
+import { defineComponent } from "vue"
 <template>
   <div class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
     <router-view />
   </div>
 </template>
 <script lang="ts">
-export default {
+import { defineComponent } from 'vue'
+
+export default defineComponent({
   setup() {
     console.log('app created')
   },
-}
+})
 </script>

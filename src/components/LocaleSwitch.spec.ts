e5f09e233c8c5f16b11816e9bc8a61e2b503bbfb
CoolSnow <670696652@qq.com>
2022-02-08 23:08:44 +0800

    feat: test component

---
+++ src/components/LocaleSwitch.spec.ts
@@ -0,0 +1,14 @@
+import { mount } from '@vue/test-utils'
+import LocaleSwitch from './LocaleSwitch.vue'
+import { createI18nWithLocale } from '@/locale'
+import store from '@/store'
+
+test('<LocaleSwitch />', () => {
+  const i18n = createI18nWithLocale('en')
+  const wrapper = mount(LocaleSwitch, {
+    global: {
+      plugins: [i18n, store],
+    },
+  })
+  expect(wrapper.find('[value="en"]').text()).toBe('en')
+})
